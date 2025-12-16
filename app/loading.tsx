f8eb78348c2ce9fa899d2c0d5681af4fe36116cb
Md Suny Shaikh <mdsunyshaikh@gmail.com>
2025-12-16 17:15:47 +0600

    deploy on vercel, add not-found and loading page

---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+import { Spinner } from "@/components/ui/spinner"
+
+const LoadingPage = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-xl">
+        <Spinner /> Loading...
+      </h1>
+    </div>
+  )
+}
+
+export default LoadingPage
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-xl font-bold">404 not found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
